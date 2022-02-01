d01cb9436646d59ba7c69fb57325fc4c17cb420f
unknown <mishrakripanshu303@gmail.com>
2022-02-01 20:59:35 +0530

    fds

---
+++ i.js
@@ -0,0 +1,9 @@
+console.log('js is started')
+let btn1 = document.createElement('h1');
+console.log('this is js loaded')
+alert('hi')
+console.log('this is js loaded')   
+var text = document.createTextNode('this is something defer')
+btn1.appendChild(text)
+document.getElementsByTagName("body")[0].appendChild(btn1)
+
