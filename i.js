40e27a550c1c7ac3973441ab72937a30d40477c6
kripanshu mishra <mishrakripanshu303@gmail.com>
2022-04-09 11:57:45 +0530

    changed whole design

--- i.js
+++
@@ -1,9 +0,0 @@
-console.log('js is started')
-let btn1 = document.createElement('h1');
-console.log('this is js loaded')
-alert('hi')
-console.log('this is js loaded')   
-var text = document.createTextNode('this is something defer')
-btn1.appendChild(text)
-document.getElementsByTagName("body")[0].appendChild(btn1)
-
